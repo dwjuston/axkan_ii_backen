65d46731eff174b70656cca5b57ffde3ec6d340e
dwjuston <wd286@georgetown.edu>
2025-04-04 20:51:22 -0400

    ready for railway deploy

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import asyncio
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import APIKeyHeader
-
 from api import app
-import logging
-import sys
+
 
 
 # Add CORS middleware
@@ -22,6 +19,7 @@ app.add_middleware(
 if __name__ == "__main__":
     asyncio.get_event_loop().set_debug(True)
 
+
     uvicorn.run(
         "main:app",
         host="127.0.0.1",  # Changed from "localhost" to "127.0.0.1" for better WebSocket support
@@ -29,6 +27,6 @@ if __name__ == "__main__":
         reload=True,  # Enable auto-reload for development
         ws="auto",
         loop="asyncio",
-        # ws="auto",  # Enable WebSocket support
         log_level="info"  # Set uvicorn log level to info
     )
+
